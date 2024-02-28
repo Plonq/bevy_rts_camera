6a634f9e562860d932999ef5f68d13cd74539424
Huon Imberger <huon@plonq.org>
2024-02-28 20:13:12 +1100

    Fix brokenness

--- src/lib.rs
+++ src/lib.rs
@@ -30,15 +30,15 @@ impl Plugin for RtsCameraPlugin {
             .add_systems(
                 Update,
                 (
-                    // (
-                    //     // initialize,
-                    //     zoom,
-                    //     follow_ground,
-                    //     update_eye_transform,
-                    //     pan.run_if(|q: Query<&CameraLock>| q.is_empty()),
-                    //     lock.run_if(|q: Query<&CameraLock>| !q.is_empty()),
-                    //     rotate,
-                    // ),
+                    (
+                        // initialize,
+                        zoom,
+                        follow_ground,
+                        update_eye_transform,
+                        pan.run_if(|q: Query<&CameraLock>| q.is_empty()),
+                        lock.run_if(|q: Query<&CameraLock>| !q.is_empty()),
+                        rotate,
+                    ),
                     move_towards_target,
                 )
                     .chain()
@@ -241,21 +241,21 @@ fn zoom(
 fn update_eye_transform(
     config: Res<CameraConfig>,
     state: Res<CameraState>,
-    rts_camera: Query<&Children, Without<CameraEye>>,
-    mut rts_cam_eye: Query<&mut Transform, With<CameraEye>>,
+    pivot_children_q: Query<&Children, With<CameraPivot>>,
+    mut eye_q: Query<&mut Transform, With<CameraEye>>,
     time: Res<Time>,
 ) {
     if !config.enabled {
         return;
     }
 
-    for children in rts_camera.iter() {
+    for children in pivot_children_q.iter() {
         for child in children {
-            if let Ok(mut eye_tfm) = rts_cam_eye.get_mut(*child) {
-                eye_tfm.rotation = Quat::from_rotation_x(config.angle - 90f32.to_radians());
+            if let Ok(mut eye) = eye_q.get_mut(*child) {
+                eye.rotation = Quat::from_rotation_x(config.angle - 90f32.to_radians());
                 let camera_offset =
                     (config.height_max.lerp(config.height_min, state.zoom)) * config.angle.tan();
-                eye_tfm.translation.z = eye_tfm.translation.z.lerp(
+                eye.translation.z = eye.translation.z.lerp(
                     camera_offset,
                     1.0 - config.smoothness.powi(7).powf(time.delta_seconds()),
                 );
@@ -269,7 +269,7 @@ fn pan(
     config: Res<CameraConfig>,
     controls: Res<CameraControls>,
     mut state: ResMut<CameraState>,
-    rts_camera: Query<&Transform>,
+    pivot_q: Query<&Transform, With<CameraPivot>>,
     button_input: Res<ButtonInput<KeyCode>>,
     mouse_input: Res<ButtonInput<MouseButton>>,
     primary_window_q: Query<&Window, With<PrimaryWindow>>,
@@ -279,21 +279,21 @@ fn pan(
         return;
     }
 
-    for rts_cam_tfm in rts_camera.iter() {
+    for pivot in pivot_q.iter() {
         let mut delta = Vec3::ZERO;
 
         // Keyboard pan
         if button_input.pressed(controls.key_up) {
-            delta += Vec3::from(rts_cam_tfm.forward())
+            delta += Vec3::from(pivot.forward())
         }
         if button_input.pressed(controls.key_down) {
-            delta += Vec3::from(rts_cam_tfm.back())
+            delta += Vec3::from(pivot.back())
         }
         if button_input.pressed(controls.key_left) {
-            delta += Vec3::from(rts_cam_tfm.left())
+            delta += Vec3::from(pivot.left())
         }
         if button_input.pressed(controls.key_right) {
-            delta += Vec3::from(rts_cam_tfm.right())
+            delta += Vec3::from(pivot.right())
         }
 
         // Edge pan
@@ -305,19 +305,19 @@ fn pan(
                     let pan_width = win_h * config.edge_pan_width;
                     // Pan left
                     if cursor_position.x < pan_width {
-                        delta += Vec3::from(rts_cam_tfm.left())
+                        delta += Vec3::from(pivot.left())
                     }
                     // Pan right
                     if cursor_position.x > win_w - pan_width {
-                        delta += Vec3::from(rts_cam_tfm.right())
+                        delta += Vec3::from(pivot.right())
                     }
                     // Pan up
                     if cursor_position.y < pan_width {
-                        delta += Vec3::from(rts_cam_tfm.forward())
+                        delta += Vec3::from(pivot.forward())
                     }
                     // Pan down
                     if cursor_position.y > win_h - pan_width {
-                        delta += Vec3::from(rts_cam_tfm.back())
+                        delta += Vec3::from(pivot.back())
                     }
                 }
             }
@@ -332,22 +332,22 @@ fn pan(
 fn lock(
     config: Res<CameraConfig>,
     mut state: ResMut<CameraState>,
-    target: Query<(&Transform, &CameraLock), Without<CameraPivot>>,
+    target_q: Query<&Transform, With<CameraLock>>,
 ) {
     if !config.enabled {
         return;
     }
 
-    for (target_tfm, _lock) in target.iter() {
-        state.target.x = target_tfm.translation.x;
-        state.target.z = target_tfm.translation.z;
+    for target in target_q.iter() {
+        state.target.x = target.translation.x;
+        state.target.z = target.translation.z;
     }
 }
 
 fn follow_ground(
     config: Res<CameraConfig>,
     mut state: ResMut<CameraState>,
-    rts_camera: Query<&Transform>,
+    pivot_q: Query<&Transform, With<CameraPivot>>,
     ground_q: Query<Entity, With<Ground>>,
     mut gizmos: Gizmos,
     mut raycast: Raycast,
@@ -356,10 +356,10 @@ fn follow_ground(
         return;
     }
 
-    for rts_cam_tfm in rts_camera.iter() {
+    for pivot in pivot_q.iter() {
         // todo: add more rays to smooth transition between sudden ground height changes???
         // Ray starting directly above where the camera is looking, pointing straight down
-        let ray1 = Ray3d::new(rts_cam_tfm.translation, Vec3::from(rts_cam_tfm.down()));
+        let ray1 = Ray3d::new(pivot.translation, Vec3::from(pivot.down()));
         let hits1 = raycast.debug_cast_ray(
             ray1,
             &RaycastSettings {
@@ -379,7 +379,7 @@ fn follow_ground(
 fn rotate(
     config: Res<CameraConfig>,
     controls: Res<CameraControls>,
-    mut rts_camera: Query<&mut Transform>,
+    mut pivot_q: Query<&mut Transform, With<CameraPivot>>,
     mouse_input: Res<ButtonInput<MouseButton>>,
     mut mouse_motion: EventReader<MouseMotion>,
     primary_window_q: Query<&Window, With<PrimaryWindow>>,
@@ -388,14 +388,14 @@ fn rotate(
         return;
     }
 
-    for mut rts_cam_tfm in rts_camera.iter_mut() {
+    for mut pivot in pivot_q.iter_mut() {
         if mouse_input.pressed(controls.button_rotate) {
             let mouse_delta = mouse_motion.read().map(|e| e.delta).sum::<Vec2>();
             if let Ok(primary_window) = primary_window_q.get_single() {
                 // Adjust based on window size, so that moving mouse entire width of window
                 // will be one half rotation (180 degrees)
                 let delta_x = mouse_delta.x / primary_window.width() * PI;
-                rts_cam_tfm.rotate_local_y(-delta_x);
+                pivot.rotate_local_y(-delta_x);
             }
         }
     }
@@ -404,11 +404,11 @@ fn rotate(
 fn move_towards_target(
     state: Res<CameraState>,
     config: Res<CameraConfig>,
-    mut rts_camera: Query<&mut Transform>,
+    mut pivot_q: Query<&mut Transform, With<CameraPivot>>,
     time: Res<Time>,
 ) {
-    for mut rts_cam_tfm in rts_camera.iter_mut() {
-        rts_cam_tfm.translation = rts_cam_tfm.translation.lerp(
+    for mut pivot in pivot_q.iter_mut() {
+        pivot.translation = pivot.translation.lerp(
             state.target,
             1.0 - config.smoothness.powi(7).powf(time.delta_seconds()),
         );
