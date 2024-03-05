f28c36ff7670c8c9250d5ccbc398913412fe3ee5
Huon Imberger <huon@plonq.org>
2024-03-05 18:05:46 +1100

    removed unused const

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,6 @@ use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 use bevy_mod_raycast::prelude::{IntersectionData, Raycast, RaycastSettings};
 
-const BASE_SPEED: f32 = 5.0;
-
 /// Bevy plugin that provides RTS camera controls.
 /// # Example
 /// ```no_run
@@ -297,7 +295,6 @@ fn pan(
     let new_target = target_tfm.0.translation
         + delta.normalize_or_zero()
             * time.delta_seconds()
-            // * BASE_SPEED
             * config.pan_speed
             // Scale based on zoom so it (roughly) feels the same speed at different zoom levels
             * target_zoom.0.remap(0.0, 1.0, 1.0, 0.5);
