7f5fd44da50e4337f8f9ee640f6c5df1eadedf78
Huon Imberger <huon@plonq.org>
2024-04-21 13:45:32 +1000

    Revert basic

--- examples/basic.rs
+++ examples/basic.rs
@@ -86,10 +86,7 @@ fn setup(
     // Camera
     commands.spawn((
         Camera3dBundle::default(),
-        RtsCamera {
-            smoothness: 0.0,
-            ..default()
-        },
+        RtsCamera::default(),
         RtsCameraControls::default(),
     ));
 }
